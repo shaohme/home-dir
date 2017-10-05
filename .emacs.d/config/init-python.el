1e00fb88b5159123cc7ea4f767a6562262416fc7
Martin Kjær Jørgensen <martin@touchpeak.com>
2017-10-05 15:53:00 +0200

    python editing

--- .emacs.d/config/init-python.el
+++ .emacs.d/config/init-python.el
@@ -14,9 +14,7 @@
 
 (setq
  ;; company-emacs-eclim-ignore-case t
- flycheck-python-pylint-executable "/usr/bin/pylint3"
- flycheck-python-pycompile-executable "/usr/bin/python3"
- flycheck-python-flake8-executable "/usr/bin/flake8"
+ flycheck-python-pycompile-executable "python3"
  flycheck-json-python-json-executable "python3"
  python-indent-guess-indent-offset nil
  python-indent-offset 4
@@ -27,6 +25,10 @@
  jedi:environment-root "default"
  jedi:server-command (list (concat python-environment-directory "/" jedi:environment-root "/bin/jediepcserver"))
  jedi:complete-on-dot t
+ elpy-rpc-backend "jedi"
+ elpy-rpc-python-command "python3"
+ flymake-error-bitmap (quote (exclamation-mark flycheck-error))
+ flymake-warning-bitmap (quote (question-mark flycheck-warning))
  )
 
 ;;; Python mode
@@ -37,20 +39,18 @@
   (add-to-list 'company-backends 'company-jedi)
   (company-mode t)
   ;; (company-quickhelp-mode t)
-  ;; (flycheck-mode t)
- (setq elpy-rpc-backend "jedi"
-       elpy-rpc-python-command "python3"
-       flymake-error-bitmap (quote (exclamation-mark flycheck-error))
-       flymake-warning-bitmap (quote (question-mark flycheck-warning))
-       flycheck-python-flake8-executable nil
-       flycheck-python-pycompile-executable "python3"
-       flycheck-python-pylint-executable nil
-       )
-  (elpy-mode t)
+  (flycheck-mode t)
+  ;; (elpy-mode t)
   (eldoc-mode t)
   (projectile-mode t))
 
 (add-hook 'python-mode-hook 'init-python-mode)
+
+(defun init-elpy-mode()
+  (local-set-key (kbd "C-c C-k") 'comment-region)
+  )
+
+(add-hook 'elpy-mode-hook 'init-elpy-mode)
 
 (provide 'init-python)
 ;;; init-python.el ends here
