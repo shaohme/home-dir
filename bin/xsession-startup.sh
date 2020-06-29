32a9b79f47f1bd0ce60b57ebab06cfac1327a2f0
Martin Kjær Jørgensen <mkj@gotu.dk>
2020-06-29 16:36:18 +0200

    more x11 and i3

---
+++ bin/xsession-startup.sh
@@ -0,0 +1,35 @@
+#!/bin/dash
+# set -e
+
+pkill picom
+# pkill dunst
+# pkill stalonetray
+pkill urxvtd
+pkill steam
+pkill clipit
+pkill syncthing-gtk
+# pkill xscreensaver
+pkill nm-applet
+# /usr/bin/emacsclient --eval "(kill-emacs)"
+
+picom -CGb
+urxvtd -q -o -f
+xsetroot -cursor_name left_ptr
+xset s off
+# xmodmap ~/.Xmodmap
+ibus-daemon -rdx
+# dunst &
+# clipit &
+diodon &
+# stalonetray &
+
+# run daemon from within X session as systemd seems
+# to leave evironment variables for one thing
+# /usr/bin/emacs --daemon &
+syncthing-gtk &
+nm-applet &
+radeon-profile &
+flameshot &
+# xscreensaver &
+
+# steam -silent &
